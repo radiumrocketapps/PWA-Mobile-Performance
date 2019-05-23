8630cc67c4b707d8a5d4882eaff963ab8f54eb31
Lucas Malvasia <lucas.malvasia@gmail.com>
2019-05-23 12:37:36 -0300

    Initial commit

---
+++ build/precache-manifest.e5c3a684fdaacc1e35a6e4813738a2e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433b8de690bc64acf9d1c54f1bd38936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2449e2ed39821b72c5fb",
+    "url": "/static/css/main.f4a84f86.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2449e2ed39821b72c5fb",
+    "url": "/static/js/main.59746f79.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78f53a704790ec690d3e3c178a2e47f4",
+    "url": "/static/media/logo.78f53a70.png"
+  }
+]);
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import logo from './static/images/logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Performance Mobile</h1>
+      <img src={logo} alt="Logo" />
+      <h2>Progressive Web App</h2>
+    </div>
+  )
+}
+
+export default App
