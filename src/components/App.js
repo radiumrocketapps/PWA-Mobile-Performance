bcfdfeec27de6ce4130ce0ffecd6f8e8093a5d16
Mauro Falcone <maurofalcone@Macbook-Pro-MF.local>
2019-05-27 16:20:08 -0300

    board and cells

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,53 @@
 import React from 'react'
+import Cell from './Cell'
 import './App.css'
-import logo from './static/images/logo.png'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Performance Mobile</h1>
-      <img src={logo} alt="Logo" />
-      <h2>Progressive Web App</h2>
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      columnsRows: 0,
+    }
+  }
+
+  handleClick = () => {
+    // do something
+  }
+  
+  handleChange = (e) => {
+    e.persist()
+    console.log(e)
+    this.setState({ columnsRows:e.target.value })
+  }
+  
+  showCells = () => {
+    const { columnsRows } = this.state
+    let list = []
+    for (let i = 0; i < columnsRows; i++) {
+      list.push(i)
+      console.log(list)
+    }
+  
+    return list.map((item, index) => {
+      return <Cell key={index} number={item}/>
+    })
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <div className="board">
+          { this.showCells() }
+        </div>
+        <div>
+          <button onClick={this.handleClick} className="play">Play</button>
+        </div>
+        <div>
+          <input type="number" onChange={this.handleChange}/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
