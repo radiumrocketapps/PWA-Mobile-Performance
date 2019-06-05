8fddf98e3d85761358ea06df802087489bab17a9
Lucas Malvasia <lucas.malvasia@gmail.com>
2019-06-04 23:02:04 -0300

    add cell component

---
+++ src/components/Game/Cell/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './cell.css'
+
+class Cell extends Component {
+  render() {
+    return <div className="cell-container" />
+  }
+}
+
+export default Cell
