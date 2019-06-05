6eea8d42b995fed4da23b81c065ce22d1f1d09e7
Lucas Malvasia <lucas.malvasia@gmail.com>
2019-06-04 22:58:38 -0300

    handle column and row change events

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -9,17 +9,40 @@ class Game extends Component {
       rows: 5
     }
   }
+
+  handleRowChange = event => {
+    this.setState({
+      rows: event.target.value
+    })
+  }
+
+  handleColumnChange = event => {
+    this.setState({
+      columns: event.target.value
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
         <div className="inputs-container">
           <label className="label">
             Rows:
-            <input className="input" type="text" value={this.state.rows} />
+            <input
+              className="input"
+              type="text"
+              value={this.state.rows}
+              onChange={this.handleRowChange}
+            />
           </label>
           <label className="label">
             Columns:
-            <input className="input" type="text" value={this.state.columns} />
+            <input
+              className="input"
+              type="text"
+              value={this.state.columns}
+              onChange={this.handleColumnChange}
+            />
           </label>
         </div>
         <div className="buttons-container">
